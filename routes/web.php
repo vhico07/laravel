5e917a6bddcda271016558b208ad8b06cf620fea
vhico <vhico pratama>
2020-08-19 21:26:14 +0700

    edit routes

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/test', function () {
+    return "test";
+});
 
 Route::get('/status', function () {
     $dbstatus = "";
